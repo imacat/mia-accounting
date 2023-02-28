2e9bf382fb3bdcd1d87a40cb838e41d4826b0e1e
依瑪貓 <imacat@mail.imacat.idv.tw>
2023-02-28 09:31:46 +0800

    Revised the documentation of the "accounting.transaction.dispatcher" module.

--- src/accounting/transaction/dispatcher.py
+++ src/accounting/transaction/dispatcher.py
@@ -14,7 +14,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-"""The transaction type dispatcher.
+"""The view dispatcher for different transaction types.
 
 """
 import typing as t
