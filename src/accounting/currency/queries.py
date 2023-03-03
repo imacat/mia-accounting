8cf81b5459b4f6c3cdf93d03069d7f46237d1e91
依瑪貓 <imacat@mail.imacat.idv.tw>
2023-03-03 23:09:20 +0800

    Revised the documentation of the "accounting.account.queries", "accounting.base_account.queries", "accounting.currency.queries", and "accounting.transaction.queries" modules.

--- src/accounting/currency/queries.py
+++ src/accounting/currency/queries.py
@@ -14,7 +14,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-"""The currency queries.
+"""The queries for the currency management.
 
 """
 import sqlalchemy as sa
