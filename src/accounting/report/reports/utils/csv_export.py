6d1e705e4b1964becac24bb00d4e7e188c648be9
依瑪貓 <imacat@mail.imacat.idv.tw>
2023-03-09 10:49:42 +0800

    Revised the documentation of the "accounting.report.reports.utils.csv_export" module.

--- src/accounting/report/reports/utils/csv_export.py
+++ src/accounting/report/reports/utils/csv_export.py
@@ -14,7 +14,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-"""The utility to export the report as CSV for download.
+"""The utilities to export the report as CSV for download.
 
 """
 import csv
