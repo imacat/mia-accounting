86c5b91697891b394c251a19fa337534ca9392f9
依瑪貓 <imacat@mail.imacat.idv.tw>
2023-04-06 08:43:14 +0800

    Advanced to version 1.0.1.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../src/'))
 project = 'Mia! Accounting'
 copyright = '2023, imacat'
 author = 'imacat'
-release = '1.0.0'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
